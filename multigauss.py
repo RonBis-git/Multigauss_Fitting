e1baeda744242f4c332bc86e40ef84044842b1bd
RonBis-git <rohanbiswas1993@gmail.com>
2024-03-02 17:54:09 +0000

    Introduced main function and read_file
    	read_file() reads the data file and unpacks into three columns of data.
    	main() function is created to handle events. This time, it calls the read_file() and stores the data columns.

--- multigauss.py
+++ multigauss.py
@@ -1,5 +1,12 @@
 import numpy as np
 from scipy.optimize import curve_fit
 
-if __name__ == 'main':
-    ...
+def read_file():
+    fname = '/home/rohan/sn/32S_89Y.dat'
+    return np.loadtxt(fname, unpack=True)
+
+def main():
+    E_cm, f_xs, err_f_xs = read_file()
+
+if __name__ == '__main__':
+    main()

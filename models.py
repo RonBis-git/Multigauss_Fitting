de27aeafa6d1515cd2745a5654479832b9299e07
RonBis-git <rohanbiswas1993@gmail.com>
2024-03-14 00:01:48 +0000

    Added dataclass  BarrierDistributionModel. Removed get_Z function.
    	dataclass BarrierDsitributionModel contains the model parameters. It has a method to return the barrier distribution as a gaussian model.
    	get_Z() function has been removed from ExcitationFunctionModel dataclass and is now been calculated inside the model functions.

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class ExcitationFunction:
     sigma: np.ndarray
     sigma_error: np.ndarray
 
+
 @dataclass
 class ExcitationFunctionModel:
     experimental_data: ExcitationFunction
@@ -18,13 +19,9 @@ class ExcitationFunctionModel:
     Wg: float
     Rg: float
 
-    @staticmethod
-    def get_Z(E_cm: np.ndarray, Vg: float, Wg: float) -> np.ndarray:
-        return (E_cm - Vg) / np.sqrt(2.0) / Wg
-
     def expression_xs(self):
         # calculating Z
-        Z = ExcitationFunction.get_Z(self.experimental_data.E_cm, self.Vg, self.Wg)
+        Z = (self.experimental_data.E_cm - self.Vg) / (np.sqrt(2.0) * self.Wg)
 
         # calculating terms for the cross-section
         bracket_term1 = np.sqrt(np.pi) * Z * erfc(-Z)
@@ -38,6 +35,7 @@ class ExcitationFunctionModel:
 
         return self.weight * outside_bracket * (bracket_term1 + bracket_term2)
 
+
 @dataclass
 class BarrierDistribution:
     E_bd: np.ndarray
@@ -46,28 +44,52 @@ class BarrierDistribution:
 
     @staticmethod
     def get_BD_from_xs(E, sigma, sigma_err):
-        Esigma = E*sigma
+        Esigma = E * sigma
 
-        fbd = np.empty(len(E)-2)
-        fbd_err = np.empty((len(E)-2))
+        fbd = np.empty(len(E) - 2)
+        fbd_err = np.empty((len(E) - 2))
         Ebd = E[1:-1]
 
-        for i in range(1,len(E)-1):
-            fbd[i-1] = 2/(E[i+1]-E[i-1])*((Esigma[i+1]-Esigma[i])/(E[i+1]-E[i])-(Esigma[i]-Esigma[i-1])/(E[i]-E[i-1]))
-            fbd_err[i-1] = Ebd[i-1]/(E[i+1]-E[i-1])**2*np.sqrt(sigma_err[i+1]**2+4*sigma_err[i]**2+sigma_err[i-1]**2)
-        
+        for i in range(1, len(E) - 1):
+            fbd[i - 1] = (
+                2
+                / (E[i + 1] - E[i - 1])
+                * (
+                    (Esigma[i + 1] - Esigma[i]) / (E[i + 1] - E[i])
+                    - (Esigma[i] - Esigma[i - 1]) / (E[i] - E[i - 1])
+                )
+            )
+            fbd_err[i - 1] = (
+                Ebd[i - 1]
+                / (E[i + 1] - E[i - 1]) ** 2
+                * np.sqrt(
+                    sigma_err[i + 1] ** 2
+                    + 4 * sigma_err[i] ** 2
+                    + sigma_err[i - 1] ** 2
+                )
+            )
+
         return Ebd, fbd, fbd_err
 
-# def expression_bd(E_bd, weight, Vg, Wg):
 
-#   Z = get_Z(E_bd,Vg,Wg)
+@dataclass
+class BarrierDistributionModel:
+    experimental_data: BarrierDistribution
+    weight: float
+    Vg: float
+    Wg: float
 
-#   # calculating terms for the barrier distribution
-#   bracket_term = np.exp(-Z*Z)
-# #   print(bracket_term)
-#   coeff = weight/(np.sqrt(2*np.pi)*Wg)
+    def expression_bd(self):
 
-#   return coeff*bracket_term
+        Z = (self.experimental_data.E_bd - self.Vg) / (np.sqrt(2.0) * self.Wg)
+
+        # calculating terms for the barrier distribution
+        bracket_term = np.exp(-Z * Z)
+        #   print(bracket_term)
+        coeff = 1.0 / (np.sqrt(2 * np.pi) * self.Wg)
+
+        return self.weight * coeff * bracket_term
+
 
 # def get_chi2(x, y_expt, y_err, y_model):
 #     return np.sum((y_expt - y_model) ** 2 / y_err**2) / len(x)
